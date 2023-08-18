5a8435731387baa06a71ed3ab8f60ae0c6ee0438
joycewanjiku <joycemumbiiwanjiku@gmail.com>
2023-08-18 16:58:05 +0300

    first commit

--- app/json.tsx
+++ app/json.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
-import Footer from './Footer/footer'
+import Footer from './footer'
 import "tailwindcss/tailwind.css";
-import NavBar from './NavBar/navBar';
-import Home from './Home/home';
-import Service from './Service/service'
-import Offer from './Offer/offer'
-import Menu from './Menu/menu';
+import NavBar from './navBar';
+import Home from './home';
+import Service from './service'
+import Offer from './offer'
+import Menu from './menu';
 
 export default function Page() {
   return (
